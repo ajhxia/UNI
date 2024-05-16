8749f0c1e29202abe42315c4853bbf9f915a504e
Alessia Cassetta <a.cassetta@ips-intelligence.com>
2024-05-16 16:05:19 +0200

    images

--- Primo Anno/Metodologie di Programmazione/Project-JPokeBattle/src/Battle/BattleFrame.java
+++ Primo Anno/Metodologie di Programmazione/Project-JPokeBattle/src/Battle/BattleFrame.java
@@ -3,6 +3,7 @@ package Battle;
 import javax.swing.*;
 
 import Game.Coach;
+import Shared.*;
 
 public class BattleFrame extends JFrame{
     JFrame frame; 
@@ -10,10 +11,23 @@ public class BattleFrame extends JFrame{
         frame = new JFrame("Battle");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(850, 470);
+
+        // Carica l'immagine di sfondo
+        ImageIcon backgroundImage = new ImageIcon(RelativePath.getAbsolutePath("/Image/battle.jpeg"));
+
+        // Crea un JLabel con l'immagine di sfondo
+        JLabel backgroundLabel = new JLabel(ImageUtility.resizeIcon(backgroundImage, 850, 470));
+
+        // Aggiunge il JLabel al JFrame
+        frame.add(backgroundLabel);
         
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
 
     }
-    
+
+    public static void main(String[] args) {
+        // Esempio di utilizzo
+        new BattleFrame(null, null);
+    }
 }
