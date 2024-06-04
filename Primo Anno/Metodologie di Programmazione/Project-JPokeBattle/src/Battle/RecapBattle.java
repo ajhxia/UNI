cd917d637544c061775dd6bad707cdb34f3f13e3
Alessia Cassetta <a.cassetta@ips-intelligence.com>
2024-06-04 11:58:23 +0200

    add recapBattle

--- Primo Anno/Metodologie di Programmazione/Project-JPokeBattle/src/Battle/RecapBattle.java
+++ Primo Anno/Metodologie di Programmazione/Project-JPokeBattle/src/Battle/RecapBattle.java
@@ -1,10 +1,37 @@
 package Battle;
 
-import javax.swing.JFrame;
+import java.awt.Color;
+
+import javax.swing.*;
+
+import Shared.PixelFont;
 
 public class RecapBattle extends JFrame{
     
+    public JFrame frame;
+
     public RecapBattle() {
-        //TODO
+        frame = new JFrame();
+        frame.setTitle("Recap Battle");
+        frame.setSize(500, 500);
+        
+        frame.setLayout(null);
+
+        JLabel labelWon = new JLabel("You won!");
+        labelWon.setFont(PixelFont.myCustomFont.deriveFont(18f));
+        labelWon.setForeground(Color.BLACK);
+        labelWon.setBounds(175, 0, 200, 50);
+        frame.add(labelWon);
+      
+
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        
+    }
+
+    public static void main(String[] args) {
+        PixelFont.loadCustomFont();
+        new RecapBattle();
     }
 }
