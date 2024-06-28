6870b0403e920bfff523fc8ee58eb47c14a0b239
ajhxia <c.alessia04@gmail.com>
2024-06-28 14:31:36 +0200

    changes

--- Primo Anno/Architettura degli Elaboratori/Esercizi/Reverse_bit.asm
+++ Primo Anno/Architettura degli Elaboratori/Esercizi/Reverse_bit.asm
@@ -16,14 +16,10 @@ for:
 	beqz $t3, fine		
 	sll $t1, $t1, 1		# Shift a sinistra di $t1 (per fare spazio al prossimo bit)
 	andi $t4, $t0, 1	# Ottiene il bit meno significativo di $t0
-	li $v0, 1           
-    	move $a0, $t4       
-    	syscall
-	
 	or $t1, $t1, $t4	# Aggiunge il bit a $t1 (costruendo il numero invertito)
 	srl $t0, $t0, 1		# Shift a destra di $t0 (per processare il prossimo bit)
 	subi $t3, $t3, 1	# Decrementa il contatore del ciclo
-	j for			# Cicla finché non sono stati processati tutti e 8 i bit
+	j for				# Cicla finche non sono stati processati tutti e 8 i bit
 	
 fine:	
 	li $v0, 1           
