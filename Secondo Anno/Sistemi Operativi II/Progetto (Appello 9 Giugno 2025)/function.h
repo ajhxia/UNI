4cccca897b8312c6a9616ea14be219ea324f1317
Alessia Cassetta <a.cassetta@ips-intelligence.com>
2025-05-14 11:33:58 +0200

    add function to #define

--- Secondo Anno/Sistemi Operativi II/Progetto (Appello 9 Giugno 2025)/function.h
+++ Secondo Anno/Sistemi Operativi II/Progetto (Appello 9 Giugno 2025)/function.h
@@ -4,6 +4,7 @@
 
 #ifndef LETTURA_FILE_H
 #define LETTURA_FILE_H
+
 typedef struct {
     double re;
     double im;
@@ -16,13 +17,20 @@ typedef struct {
 } InitValue;
 
 typedef struct {
+    char name;
     ComplexNumber *value;
     int count_n;
-}CircDef;
+} Gate;
+
+typedef struct {
+    Gate *gates;
+    int count_n;
+} CircDef;
 
 char *read_file_and_print_lines(char *filename);
 InitValue split_function_init(char *var);
-void split_function_define_circle(char *var);
+CircDef split_function_define_circle(char *var);
 void free_init_value(InitValue *iv);
+
 
 #endif //LETTURA_FILE_H
