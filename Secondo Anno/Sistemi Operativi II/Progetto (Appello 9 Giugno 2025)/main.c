b7f23c71ffb30adc6efe4f07410bd154434661d2
Alessia Cassetta <a.cassetta@ips-intelligence.com>
2025-05-22 11:25:20 +0200

    operation

--- Secondo Anno/Sistemi Operativi II/Progetto (Appello 9 Giugno 2025)/main.c
+++ Secondo Anno/Sistemi Operativi II/Progetto (Appello 9 Giugno 2025)/main.c
@@ -3,24 +3,58 @@
 #include "file_reader.h"
 #include "operation.h"
 #include "utility.h"
-
-int main(){
-    char *filename1 = "init-ex.txt";
+int load_and_parse_data(char *filename1, char *filename2, InitValue *init, CircuitDef *circuit) {
     char *file1 = read_file_and_print_lines(filename1);
-
-    char *filename2 = "circ-ex.txt";
     char *file2 = read_file_and_print_lines(filename2);
 
-    if (file1 == NULL) {
+    if (!file1 || !file2) {
+        free(file1);
+        free(file2);
+        return 0;
+    }
+
+    *init = split_function_init(file1);
+    *circuit = split_function_define_circle(file2);
+
+    free(file1);
+    free(file2);
+
+    if (init->count_n == 0 || circuit->count_n == 0 || circuit->circ_len == 0) {
+        return 0;
+    }
+
+    return 1;
+}
+
+void run_circuit(const InitValue *init, ComplexNumber **matrix, int size) {
+    ComplexNumber *final_state = complex_matrix_vector_multiply(matrix, init->value, size);
+    printf("Stato finale:\n");
+    print_state(final_state, size);
+    free(final_state);
+}
+
+int main() {
+    InitValue init;
+    CircuitDef circuit;
+
+    if (!load_and_parse_data("init-ex.txt", "circ-ex.txt", &init, &circuit)) {
+        printf("Errore nei dati di input\n");
         return 1;
     }
 
-    if (file2 == NULL) {
+    ComplexNumber **circuit_matrix = build_total_circuit_matrix(&circuit);
+    if (!circuit_matrix) {
+        printf("Errore durante la costruzione della matrice del circuito\n");
+        free_init_value(&init);
+        free_circuit(&circuit);
         return 1;
     }
 
-    CircuitDef circ = split_function_define_circle(file2);
-    InitValue init = split_function_init(file1);
+    run_circuit(&init, circuit_matrix, circuit.gates[0].size);
+
+    free_complex_matrix(circuit_matrix, circuit.gates[0].size);
+    free_init_value(&init);
+    free_circuit(&circuit);
 
     return 0;
 }
--- Secondo Anno/Sistemi Operativi II/Progetto (Appello 9 Giugno 2025)/operation.h
+++ Secondo Anno/Sistemi Operativi II/Progetto (Appello 9 Giugno 2025)/operation.h
@@ -5,7 +5,12 @@
 #ifndef OPERATION_H
 #define OPERATION_H
 #include "file_reader.h"
-
+ComplexNumber complex_multiply(ComplexNumber a, ComplexNumber b);
+ComplexNumber complex_add(ComplexNumber a, ComplexNumber b);
 void simulate_and_print(CircuitDef def, InitValue init);
-
+ComplexNumber *apply_gate(ComplexNumber **matrix, ComplexNumber *vector, int size);
+void print_state(ComplexNumber *state, int size);
+ComplexNumber *complex_matrix_vector_multiply(ComplexNumber **matrix, ComplexNumber *vector, int size);
+void free_complex_matrix(ComplexNumber **matrix, int size);
+ComplexNumber **build_total_circuit_matrix(const CircuitDef *circuit);
 #endif //OPERATION_H
