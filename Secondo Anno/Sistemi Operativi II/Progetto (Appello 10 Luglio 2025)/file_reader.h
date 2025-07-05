7d2e88f73795fe5984a6dd5c63f59f33e472bb55
ajhxia <c.alessia04@gmail.com>
2025-07-05 15:55:53 +0200

    progetto modificato in ANSI C 89

--- Secondo Anno/Sistemi Operativi II/Progetto (Appello 10 Luglio 2025)/file_reader.h
+++ Secondo Anno/Sistemi Operativi II/Progetto (Appello 10 Luglio 2025)/file_reader.h
@@ -1,9 +1,6 @@
-//
-// Created by alebox on 22/06/25.
-//
 #ifndef FILE_READER_H
 #define FILE_READER_H
 #include "utility.h"
 InitValue parse_function_init(char *var);
 CircuitDef parse_function_define_circle(char *var);
-#endif //FILE_READER_H
+#endif /* FILE_READER_H */
